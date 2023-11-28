2b1c74f4af6a4ac2a9538b30aaa9ec09a5da91cb
Vladislav Valov <vladislav890@gmail.com>
2023-11-28 22:09:43 +0300

    add react & styles loaders

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,12 @@
+import "./style.scss";
+
+function MyButton() {
+    return (
+        <div>
+            Hello
+            <button className="button">I'm a button</button>
+        </div>
+    );
+}
+
+export default MyButton;
---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import { render } from "react-dom";
+
+import MyButton from "./components/Button/index";
+
+render(<MyButton />, document.getElementById("root"));
