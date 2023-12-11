4f80960f796a1131d62dd6cf4643cccd84f8bea5
Vladislav Valov <vladislav890@gmail.com>
2023-12-11 22:37:57 +0300

    add localization i18n

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,10 @@
+import { Suspense } from "react";
+
 import { useTheme } from "app/providers/ThemeProvider";
 import { classNames } from "shared/lib/classNames/classNames";
 import { AppRouter } from "./providers/router";
-import { Navbar } from "widgets/Navbar/Navbar";
-import { Sidebar } from "widgets/Sidebar/ui";
+import { Navbar } from "widgets/Navbar/ui/Navbar";
+import { Sidebar } from "widgets/Sidebar";
 
 import "./styles/styles.scss";
 
@@ -11,11 +13,13 @@ const App = () => {
 
     return (
         <div className={classNames("app", {}, [theme])}>
-            <Navbar />
-            <div className="content-page">
-                <Sidebar />
-                <AppRouter />
-            </div>
+            <Suspense fallback="">
+                <Navbar />
+                <div className="content-page">
+                    <Sidebar />
+                    <AppRouter />
+                </div>
+            </Suspense>
         </div>
     );
 };
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 
 import { classNames } from "shared/lib/classNames/classNames";
-import { ThemeSwitcher } from "shared/ui/ThemeSwitcher";
+import { ThemeSwitcher } from "widgets/ThemeSwitcher";
 
 import cls from "./SideBar.module.scss";
+import { LangSwitcher } from "widgets/LangSwitcher";
 
 interface SideBarProps {
     className?: string;
@@ -17,6 +18,7 @@ export const Sidebar = ({ className }: SideBarProps) => {
             <button onClick={onToggle}>Toggle</button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
+                <LangSwitcher className={cls.lang} />
             </div>
         </div>
     );
