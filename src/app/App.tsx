bd5087cab1faaaf8d54993cea9112184f7192a24
Vladislav Valov <vladislav890@gmail.com>
2024-02-28 22:59:44 +0300

    11. AppRouter and config for router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,35 +1,28 @@
-import { Suspense } from 'react';
-import { Route, Routes, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-import { MainPage } from 'pages/MainPage';
-import { AboutPage } from 'pages/AboutPage';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
+import { AppRouter } from './providers/router';
 
 import './styles/styles.scss';
 
 const App = () => {
-  const { theme, toggleTheme } = useTheme();
+    const { theme, toggleTheme } = useTheme();
 
-  return (
-    <div className={classNames('app', {}, [theme])}>
-      <Link to='/'>
-        Главная
-      </Link>
-      <Link to='/about'>
-        О сайте
-      </Link>
-      <Suspense fallback={'Loading...'}>
-        <Routes>
-          <Route path='/' element={<MainPage/>}/>
-          <Route path='/about' element={<AboutPage/>}/>
-        </Routes>
-      </Suspense>
-      <button onClick={toggleTheme}>
-        Сменить тему
-      </button>
-    </div>
-  )
+    return (
+        <div className={classNames('app', {}, [theme])}>
+            <Link to='/'>
+                Главная
+            </Link>
+            <Link to='/about'>
+                О сайте
+            </Link>
+            <AppRouter/>
+            <button onClick={toggleTheme}>
+                Сменить тему
+            </button>
+        </div>
+    )
 };
 
 export default App;
---
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -0,0 +1,22 @@
+import { Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+
+import { routerConfig } from "shared/config/routeConfig/routeConfig";
+
+export const AppRouter = () => {
+    return (
+        <Suspense fallback={'Loading...'}>
+            <Routes>
+                {Object.values(routerConfig)
+                    .map(({ path, element }) => (
+                        <Route
+                            key={path}
+                            path={path}
+                            element={element}
+                        />
+                    )
+                )}
+            </Routes>
+        </Suspense>
+    );
+};
